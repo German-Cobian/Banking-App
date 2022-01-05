c9a5ab6b3308efa3b69a1fcb48bc72243b3373ed
German Renato Cobian <germancobian@hotmail.com>
2022-01-04 19:03:05 -0800

    Implement actionCre-creators in action-creators/index.js combined them in state/index.js and passed them into the App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import {useSelector} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { bindActionCreators } from "redux"
+import { actionCreators } from "./state/index"
 
 function App() {
-  const state = useSelector((state) => state);
-  console.log(state)
+  //const state = useSelector((state) => state);
+  const account = useSelector((state) => state.account)
+
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
 
   return (
     <div className="App">
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
     </div>
   );
 }
